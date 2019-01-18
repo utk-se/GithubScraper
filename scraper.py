d0223fbf91a83e87b28453843030b52fcc53c718
Manny Bhidya <abhidya@vols.utk.edu>
2019-01-18 01:47:58 -0500

    fixed some messed up text that moved around

--- scraper.py
+++ scraper.py
@@ -12,14 +12,7 @@ HEADERS_LIST = [
     'Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13',
     'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
     'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
-    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.3
-browser = RoboBrowser(session=session, user_agent=random.choice(HEADERS_LIST), parser="lxml")
-page = 1
-url = "https://github.com/search?l=Java&o=desc&p=" + str(page) + "&q=java&s=stars&type=Repositories"
-browser.open(url)
-while len(link) < 100:
-    browser.open(url)
-88 Version/12.16',
+    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
     'Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre'
 ]
 
@@ -33,12 +26,12 @@ browser.open(url)
 while len(link) < 100:
     browser.open(url)
     results = browser.find_all("a", {"class": "v-align-middle"})
-    print('results', len(results))
+    print(len(results))
     page = page + 1
     url = "https://github.com/search?l=Java&o=desc&p=" + str(page) + "&q=java&s=stars&type=Repositories"
     for items in results:
         link.append(items)
-    print('link', len(link))
-    print('page', page)
+    print(len(link))
+    print(page)
 
 print(link)
